e70b1a05b793a3cb7fad0221b5567f065cc4d546
Mintthehole <john@John.(none)>
2013-11-12 22:13:08 +0530

    Changes in sales call enquiry

--- app/helpers/emailers_helper.rb
+++ app/helpers/emailers_helper.rb
@@ -4,7 +4,7 @@ module EmailersHelper
     if approved
     	Customer.find_all_by_approved(true).each {|v| options = options.merge({v.name => v.name})}
     else
-    	Customer.all.each {|v| options = options.merge({v.name => v.name})}
+    	Customer.order('name asc').each {|v| options = options.merge({v.name => v.name})}
     end
     options  
 	end
