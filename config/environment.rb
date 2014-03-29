56bf2a254e655513cda6c1daea402fecb0ebb989
Mintthehole <john@John.(none)>
2014-03-29 13:31:34 +0530

    changes in email order

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 EuroExim::Application.initialize!
+my_date_formats = { :default => '%d/%m/%Y' }
+Time::DATE_FORMATS.merge!(my_date_formats)
+Date::DATE_FORMATS.merge!(my_date_formats)
