49d91caefce1b3ecdc494061f7c2c20943e663ef
Mint <mintthehole@gmail.com>
2014-06-11 22:10:32 +0530

    Changes in date

--- app/controllers/bo_reports_controller.rb
+++ app/controllers/bo_reports_controller.rb
@@ -6,13 +6,9 @@ class BoReportsController < InheritedResources::Base
     @bo_report = BoReport.find_by_id(params[:id])
     params[:from_date] = Time.zone.today - 7.days if params[:from_date].blank?
     params[:to_date] = Time.zone.today if params[:to_date].blank?
-    conditions = ["id is not null"]
-    conditions[0] << " AND created_at between date(?) and date(?)"
-    conditions << params[:from_date].to_date
-    conditions << params[:to_date].to_date
-    @bookings = Booking.find(:all)#, :conditions => conditions)
-    # p @bookings
-    # p ppp
+    start = params[:from_date].to_datetime
+    d_end =  params[:to_date].to_datetime
+    @bookings = Booking.where(:created_at => start..d_end)
     respond_to do |format|
       format.html {render :layout => 'application1'}
       format.json { render json: ReportDatatable.new(view_context) }
