030c037d273fabc7e8ac4337378d56e154b8c82d
EUROEXIM <euroexim@euroexim-Compaq-620.(none)>
2014-10-12 18:17:51 +0530

    [ES14-0011] - Fixing the end-date condition for fetching bookings

--- app/controllers/bo_reports_controller.rb
+++ app/controllers/bo_reports_controller.rb
@@ -7,7 +7,7 @@ class BoReportsController < InheritedResources::Base
     params[:from_date] = Time.zone.today - 7.days if params[:from_date].blank?
     params[:to_date] = Time.zone.today if params[:to_date].blank?
     start = params[:from_date].to_datetime
-    d_end =  params[:to_date].to_datetime
+    d_end =  (params[:to_date] + 1.day).to_datetime
     @bookings = Booking.where(:created_at => start..d_end).paginate(:per_page => 100, :page => params[:page])
     respond_to do |format|
       format.html {render :layout => 'application1'}
