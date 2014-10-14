32984bbbf8a75f62d4de30841404f94f6a6c2683
EUROEXIM <euroexim@euroexim-Compaq-620.(none)>
2014-10-14 18:42:06 +0530

    [ES14-0011] - Setting the end date search field correctly

--- app/controllers/bo_reports_controller.rb
+++ app/controllers/bo_reports_controller.rb
@@ -7,11 +7,11 @@ class BoReportsController < InheritedResources::Base
     params[:from_date] = Time.zone.today - 7.days if params[:from_date].blank?
     params[:to_date] = Time.zone.today if params[:to_date].blank?
     start = params[:from_date].to_datetime
-    d_end =  (params[:to_date] + 1.day).to_datetime
+    d_end =  params[:to_date].to_datetime
     if current_user.nomination?
-      @bookings = Booking.where(:created_at => start..d_end, :is_nomination => true).paginate(:per_page => 100, :page => params[:page])
+      @bookings = Booking.where(:created_at => start..d_end.end_of_day, :is_nomination => true).paginate(:per_page => 100, :page => params[:page])
     else
-      @bookings = Booking.where(:created_at => start..d_end, :is_nomination => false).paginate(:per_page => 100, :page => params[:page])
+      @bookings = Booking.where(:created_at => start..d_end.end_of_day, :is_nomination => false).paginate(:per_page => 100, :page => params[:page])
     end
     respond_to do |format|
       format.html {render :layout => 'application1'}
