51958268810ed90469326060d962412b0cf7ca1b
EUROEXIM <euroexim@euroexim-Compaq-620.(none)>
2014-10-16 12:56:32 +0530

    [ES14-0003] - Filtering the bookings that each role can view in Booking Reports

--- app/controllers/bo_reports_controller.rb
+++ app/controllers/bo_reports_controller.rb
@@ -8,10 +8,13 @@ class BoReportsController < InheritedResources::Base
     params[:to_date] = Time.zone.today if params[:to_date].blank?
     start = params[:from_date].to_datetime
     d_end =  params[:to_date].to_datetime
-    if current_user.nomination?
-      @bookings = Booking.where(:created_at => start..d_end.end_of_day, :is_nomination => true).paginate(:per_page => 100, :page => params[:page])
+
+    if current_user.admin?
+      @bookings = Booking.where(:created_at => start..d_end.end_of_day).paginate(:per_page => 100, :page => params[:page])
+    elsif current_user.nomination?
+      @bookings = Booking.where(:created_at => start..d_end.end_of_day, :is_nomination => true, :user_id => current_user.id).paginate(:per_page => 100, :page => params[:page])
     else
-      @bookings = Booking.where(:created_at => start..d_end.end_of_day, :is_nomination => false).paginate(:per_page => 100, :page => params[:page])
+      @bookings = Booking.where(:created_at => start..d_end.end_of_day, :is_nomination => false, :user_id => current_user.id).paginate(:per_page => 100, :page => params[:page])
     end
     respond_to do |format|
       format.html {render :layout => 'application1'}
