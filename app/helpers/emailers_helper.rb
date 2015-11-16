bb51834004d06fa5cc1fdc703b76d3865adbbcc9
John <john.pollo@strata.co.in>
2015-11-16 19:42:28 +0530

    CHange

--- app/helpers/emailers_helper.rb
+++ app/helpers/emailers_helper.rb
@@ -1,10 +1,10 @@
 module EmailersHelper
-	def build_customer(approved = false)
+	def build_customer(nomination,approved = false)
     options = {'-- Please Select -- ' => ''}
     if approved
-    	Customer.find_all_by_approved(true).each {|v| options = options.merge({v.name => v.name})}
+    	Customer.where(:nomination =>nomination, :approved => approved).each {|v| options = options.merge({v.name => v.name})}
     else
-    	Customer.order('name asc').each {|v| options = options.merge({v.name => v.name})}
+    	Customer.where(:nomination =>nomination).order('name asc').each {|v| options = options.merge({v.name => v.name})}
     end
     options  
 	end
