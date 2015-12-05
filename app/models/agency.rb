4d3daf175e3e8995ed74d52058709fe914d56bcf
John <john.pollo@strata.co.in>
2015-12-05 12:28:16 +0530

    Changes in nomination

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,5 +1,5 @@
 class Agency < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name,:country,:city,:sector,:network,:pics,:phone_nos,:website,:coordinator,:coordinator_email,:notes,:approved
   validates :name, :presence => true
   validates :email, :presence => true
 end
